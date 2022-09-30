1a8ddd72ea9add741c8ba386bc0a0e38fb590692
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-30 14:24:46 +0200

    adding new file

---
+++ Student.rb
@@ -0,0 +1,13 @@
+require_relative 'person'
+
+class Students < Person
+  def initialize(_classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end

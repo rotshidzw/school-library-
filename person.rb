d7d630fa20121e47e3fdf5a3a6e11c381bd10084
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-10-01 11:00:08 +0200

    adding decorate classes

--- person.rb
+++ person.rb
@@ -1,16 +1,25 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = rand(1..10_000)
-    @name = name
+    @id = Random.rand(1..1000)
     @age = age
+    @name = name
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
